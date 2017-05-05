62c174638e5dd8e0150d2df65acccc8beec95a26
Igor <igor.peregudov93@gmail.com>
2017-05-05 18:39:28 +0200

    Add files via upload

--- portfolio/vk-stats/js/script.js
+++ portfolio/vk-stats/js/script.js
@@ -3,12 +3,80 @@ window.onload = function() {
     VK.init({
         apiId: 5884209
     });
+     
+    //ajax snipet
     
-    if(window.location.href.indexOf('access_token') === -1){
-        window.location.href = 'https://oauth.vk.com/authorize?client_id=5884209&display=popup&redirect_uri=https://igor-peregudov.github.io/portfolio/vk-stats/index.html&scope=groups&response_type=token&v=5.63&state=good';
-    }
+    var ajax = {};
+    ajax.x = function () {
+        if (typeof XMLHttpRequest !== 'undefined') {
+            return new XMLHttpRequest();
+        }
+        var versions = [
+            "MSXML2.XmlHttp.6.0",
+            "MSXML2.XmlHttp.5.0",
+            "MSXML2.XmlHttp.4.0",
+            "MSXML2.XmlHttp.3.0",
+            "MSXML2.XmlHttp.2.0",
+            "Microsoft.XmlHttp"
+        ];
+
+        var xhr;
+        for (var i = 0; i < versions.length; i++) {
+            try {
+                xhr = new ActiveXObject(versions[i]);
+                break;
+            } catch (e) {
+            }
+        }
+        return xhr;
+    };
+
+    ajax.send = function (url, callback, method, data, async) {
+        if (async === undefined) {
+            async = true;
+        }
+        var x = ajax.x();
+        x.open(method, url, async);
+        x.onreadystatechange = function () {
+            if (x.readyState == 4) {
+                callback(x.responseText)
+            }
+        };
+        if (method == 'POST') {
+            x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        }
+        x.send(data)
+    };
+
+    ajax.get = function (url, data, callback, async) {
+        var query = [];
+        for (var key in data) {
+            query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
+        }
+        ajax.send(url + (query.length ? '?' + query.join('&') : ''), callback, 'GET', null, async)
+    };
+
+    ajax.post = function (url, data, callback, async) {
+        var query = [];
+        for (var key in data) {
+            query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
+        }
+        ajax.send(url, callback, 'POST', query.join('&'), async)
+    };
     
+    //end snipet for Ajax
     
+    ajax.get('https://oauth.vk.com/authorize', {
+        client_id: 5884209,
+        display: 'popup',
+        redirect_uri: 'https://igor-peregudov.github.io/portfolio/vk-stats/index.html',
+        scope: 'groups',
+        response_type: 'token',
+        v: '5.63',
+        state: 'good'
+    }, function(res) {
+        console.log(res);
+    });
     
     var groupName;
     var groupAvatar;
