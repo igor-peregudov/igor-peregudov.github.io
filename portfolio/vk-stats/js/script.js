ac5322a34a920780df822d0f6f155dd2738a76c5
Igor <igor.peregudov93@gmail.com>
2017-05-05 19:43:15 +0200

    Add files via upload

--- portfolio/vk-stats/js/script.js
+++ portfolio/vk-stats/js/script.js
@@ -8,7 +8,9 @@ window.onload = function() {
         window.location.href = 'https://oauth.vk.com/authorize?client_id=5884209&display=popup&redirect_uri=https://igor-peregudov.github.io/portfolio/vk-stats/index.html&scope=groups&response_type=token&v=5.63&state=good';
     }
     
-    
+    VK.Api.call('stats.trackVisitor', {}, function(res) {
+        console.log(res);
+    });
     
     var groupName;
     var groupAvatar;
